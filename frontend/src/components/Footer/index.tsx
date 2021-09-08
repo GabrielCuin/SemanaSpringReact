47e7b22f2a5d787a8822bb418b9a7bd10b3cfd6d
Gabriel Cuin dos Santos <gabrielcuinprof@gmail.com>
2021-09-08 16:54:11 -0300

    Componentes básicos estaticos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/GabrielCuin" target="_blank" rel="noreferrer">Gabriel Cuin</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/g_cuin" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
