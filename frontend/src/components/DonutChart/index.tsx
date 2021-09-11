aa6f6cead343ec4574686b4152546f1d41ea1571
Gabriel Cuin dos Santos <gabrielcuinprof@gmail.com>
2021-09-11 16:11:56 -0300

    integração da Barra em donut

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -20,7 +20,7 @@ const DonutChart = () => {
                 const mySeries = data.map(x => x.sum);
 
                 setChartData({ labels: myLabels, series: mySeries });
-            })
+            }) 
     }, []);
 
     //const mockData = {
